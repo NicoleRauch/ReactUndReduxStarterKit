824981d0d486147333ca2c9dd9f03b8794d438aa
Nicole Rauch <nicole.m@gmx.de>
2016-06-18 14:51:18 +0200

    Added a backend and set up the server to proxy to it.

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ var config = require("./webpack.config");
 
 // settings for local dev server
 var localPort = 3000;
+var proxyServer = 'http://localhost:5555';
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  proxy: {
+    '*': proxyServer
+  }
 }).listen(localPort, "0.0.0.0", function (err) {
   if (err) {
     console.log(err);
